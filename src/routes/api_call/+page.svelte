5e6848016376dab03e99dfc07b52792693bc7ae1
matt <matt@dhcp-10-250-41-54.harvard.edu>
2024-11-03 14:36:43 -0500

    quick push

--- src/routes/api_call/+page.svelte
+++ src/routes/api_call/+page.svelte
@@ -8,13 +8,23 @@
 
 <!-- VAR DECLARATION -->
 <script>
+    // import ApiResponse from '$lib/components/ApiResponse.svelte';
+    // let api_response = writable('');
+
+    // (When we don't need an immediate update within our DOM, we can use a regular variable)
     let input_value = '';
     // Declare input_value with $state('') to trigger updates on frontend
     let curr_input = $state('');
 
-    function callAPI() {
-        curr_input = input_value
+    async function callAPI() {
+        if (!input_value.trim()) {
+            alert("Please enter a valid title.");
+            return;
+        }
+
+        
     }
+
 </script>
 
 <!-- INPUT FIELD / BUTTON -->
