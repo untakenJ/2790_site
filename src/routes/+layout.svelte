ccacf2a101ab98bdb9dc2a21950b9fb738c91051
Matt Kotzbauer <mkotzbauer52@gmail.com>
2024-12-02 17:40:58 -0500

    improved painting functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,10 @@
 
 <nav>
     <p><a href = './'>Home</a></p>
-    <p><a href="./base_api_call">Base API Call Demo</a></p>
     <p><a href="./image_api_call">Image API Call Demo</a></p>
     <p><a href="./image_interaction">Image Interaction</a></p>
     <p><a href="./paint_interaction">Paint Interaction</a></p>
+    <p><a href="./further_painting">Experimental Painting Functions</a></p>
 </nav>
 
 {@render children()}
